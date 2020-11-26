cee69cc1767859f0d191510f48b1c3169f3adac9
zjcfynn <58805062+zjcfynn@users.noreply.github.com>
2020-11-26 16:03:05 +0800

    Update and rename cpud.js to Toutiao.js

---
+++ Toutiao.js
@@ -0,0 +1,18 @@
+/*
+[Script]
+
+Toutiao = type=http-response,pattern=^https:\/\/api\w-normal-c-hl\.snssdk\.com\/api(\/news)?\/feed(\/thread_aggr)?\/v.*\/,requires-body=1,max-size=524288,script-path=https://Choler.github.io/Surge/Script/Toutiao.js
+
+[MITM]
+hostname = api*-normal-c-hl.snssdk.com
+*/
+
+var obj = JSON.parse($response.body);
+if (obj.data) {
+  for (var i = obj.data.length - 1; i >= 0; i--) {
+    if (obj.data[i].content.indexOf("raw_ad_data") > 0) {
+      obj.data.splice(i, 1);
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});

97ec4191ac345b9b665ef0a5425c294e64188aa6
yfue <58805062+yfue@users.noreply.github.com>
2020-12-30 17:48:36 +0800

    Update Weather_AQI.js

--- Weather_AQI.js
+++ Weather_AQI.js
@@ -3,11 +3,11 @@
 // STEP 1: 前往 https://aqicn.org/data-platform/token/ 注册账户，将申请的 API Token 填入下方
 const aqicnToken = '35b08d06b97a2b3bb3fb7dec87dec9826faf0f02'
 
-// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本
+// STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
 	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?, requires-body=true, script-path=/path/to/iOS_Weather_AQI_Standard.js
-
+	
 	[MITM]
 	hostname = weather-data.apple.com
 */
@@ -67,7 +67,7 @@ function getPrimaryPollutant(pollutant) {
 		case 'o3':
 			return 'OZONE';
 		default:
-			return "OTHER";
+			console.log('Unknown pollutant ' + pollutant);
 	}
 }
 

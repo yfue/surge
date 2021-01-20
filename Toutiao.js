d3c4b0f32e85e6f3959961948cb855bdf17ece0a
yfue <58805062+yfue@users.noreply.github.com>
2021-01-20 12:24:51 +0800

    Update Toutiao.js

--- Toutiao.js
+++ Toutiao.js
@@ -9,7 +9,7 @@ hostname = api*-normal-c-hl.snssdk.com
 var obj = JSON.parse($response.body);
 if (obj.data) {
   for (var i = obj.data.length - 1; i >= 0; i--) {
-    if (obj.data[i].content.indexOf("raw_ad_data") > 0) {
+    if (obj.data[i].content.indexOf("广告") > 0) {
       obj.data.splice(i, 1);
     }
   }

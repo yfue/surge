08457cac8993d6f993cb097d340a361ae0227833
yfue <58805062+yfue@users.noreply.github.com>
2021-04-30 14:47:44 +0800

    Update Toutiao.js

--- Toutiao.js
+++ Toutiao.js
@@ -1,6 +1,6 @@
 /*
 [Script]
-Toutiao = type=http-response,pattern=^https:\/\/api\w-normal-c-hl\.snssdk\.com\/api(\/news)?\/feed(\/thread_aggr)?\/v.*\/,requires-body=1,max-size=524288,script-path=https://github.com/yfue/surge/raw/Module/Toutiao.js
+Toutiao = type=http-response,pattern=^https:\/\/api\w-normal-hl\.toutiaoapi\.com\/api(\/news)?\/feed(\/thread_aggr)?\/v.*\/,requires-body=1,max-size=524288,script-path=https://github.com/yfue/surge/raw/Module/Toutiao.js
 
 [MITM]
 hostname = api*-normal-c-hl.snssdk.com
@@ -10,6 +10,13 @@ var obj = JSON.parse($response.body);
 if (obj.data) {
   for (var i = obj.data.length - 1; i >= 0; i--) {
     if (obj.data[i].content.indexOf("广告") > 0) {
+      obj.data.splice(i, 1);
+    }
+  }
+}
+if (obj.data) {
+  for (var i = obj.data.length - 1; i >= 0; i--) {
+    if (obj.data[i].content.indexOf("置顶") > 0) {
       obj.data.splice(i, 1);
     }
   }

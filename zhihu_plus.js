666e354abfe6d87dfabae3f667746a0181076999
yfue <58805062+yfue@users.noreply.github.com>
2021-05-21 12:45:57 +0800

    Update zhihu_plus.js

--- zhihu_plus.js
+++ zhihu_plus.js
@@ -186,7 +186,7 @@ let magicJS = MagicJS(scriptName, "INFO");
               element.hasOwnProperty("ad") ||
               // 取消以下两行注释，推荐列表拦截视频与直播
               element["extra"]["type"] === "drama" ||
-              element["extra"]["type"] === "zvideo" ||
+              element["brief"]["type"] === "zvideo" ||
               // 取消以下注释，推荐列表拦截“盐选推荐”
               IsYanXuan(element) ||
               // 注释下行，推荐列表关闭关键字屏蔽功能
